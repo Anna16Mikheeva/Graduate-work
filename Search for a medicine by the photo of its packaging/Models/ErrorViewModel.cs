5ddcb603fb7d5679d2134063f71110f22e790870
WINDOWS 10 <anna.mav31@gmail.com>
2023-03-19 15:30:20 +0700

    Добавьте файлы проекта.

---
+++ Search for a medicine by the photo of its packaging/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Search_for_a_medicine_by_the_photo_of_its_packaging.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
