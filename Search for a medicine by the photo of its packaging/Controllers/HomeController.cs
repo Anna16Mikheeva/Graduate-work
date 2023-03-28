4310356c34ab122eab14ba53c8b31da50cd6f2d9
WINDOWS 10 <anna.mav31@gmail.com>
2023-03-28 19:07:38 +0700

    Внедрение Tesseract. Редактирование кнопки "Выбор файла"

--- Search for a medicine by the photo of its packaging/Controllers/HomeController.cs
+++ Search for a medicine by the photo of its packaging/Controllers/HomeController.cs
@@ -1,21 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
 using Search_for_a_medicine_by_the_photo_of_its_packaging.Models;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using System.Threading.Tasks;
+using Tesseract;
 
 namespace Search_for_a_medicine_by_the_photo_of_its_packaging.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        //private Image image = new Image();
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IWebHostEnvironment webHostEnvironment)
         {
             _logger = logger;
+            _webHostEnvironment = webHostEnvironment;
         }
 
         public IActionResult Index()
@@ -34,9 +39,31 @@ namespace Search_for_a_medicine_by_the_photo_of_its_packaging.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
-        public string IndexF()
+        [HttpPost]
+        public string TextRecognising(Image image)
         {
-            return "hnghv";
+            //string fileName = null;
+            //if (image.PackingImage != null)
+            //{
+            //    string uploadDir = Path.Combine(_webHostEnvironment.WebRootPath, "Img");
+            //    fileName = Guid.NewGuid().ToString() + "-" + image.PackingImage.FileName;
+            //    string filePath = Path.Combine(uploadDir, fileName);
+            //    using (var fileStream = new FileStream(filePath, FileMode.Create))
+            //    {
+            //        image.PackingImage.CopyTo(fileStream);
+            //    }
+            //}
+            var img = Tes(image);
+            return img;
+        }
+
+        public string Tes(Image image)
+        {
+            var ocrengine = new TesseractEngine(@".\tessdata", "rus+eng", EngineMode.Default);
+            var img = Pix.LoadFromFile(image.PackingImage.FileName);
+            var res = ocrengine.Process(img);
+            image.DD = res.GetText();
+            return image.DD;
         }
     }
 }
---
+++ Search for a medicine by the photo of its packaging/Models/Image.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Search_for_a_medicine_by_the_photo_of_its_packaging.Models
+{
+    public class Image
+    {
+        public IFormFile PackingImage { get; set; }
+        public string DD { get; set; }
+        public Image()
+        {
+
+        }
+    }
+}
