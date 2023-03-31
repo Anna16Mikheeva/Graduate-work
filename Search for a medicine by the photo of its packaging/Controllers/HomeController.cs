de98577fa6451dce8b8664cb9bf196fc95d0c322
WINDOWS 10 <anna.mav31@gmail.com>
2023-03-31 23:23:25 +0700

    Непонятные ошибки

--- Search for a medicine by the photo of its packaging/Controllers/HomeController.cs
+++ Search for a medicine by the photo of its packaging/Controllers/HomeController.cs
@@ -1,37 +1,33 @@
-﻿using System;
-using System.IO;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Mvc;
 using Search_for_a_medicine_by_the_photo_of_its_packaging.Models;
 using System.Diagnostics;
 using System.Threading.Tasks;
 using Tesseract;
 using System.Net.Http;
+using Newtonsoft.Json;
 
 namespace Search_for_a_medicine_by_the_photo_of_its_packaging.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-        private readonly IWebHostEnvironment _webHostEnvironment;
+        //private readonly ILogger<HomeController> _logger;
+        //private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public HomeController(ILogger<HomeController> logger, IWebHostEnvironment webHostEnvironment)
-        {
-            _logger = logger;
-            _webHostEnvironment = webHostEnvironment;
-        }
-
-        //public IActionResult Index()
+        //public HomeController(ILogger<HomeController> logger, IWebHostEnvironment webHostEnvironment)
         //{
-        //    return View();
+        //    //_logger = logger;
+        //    //_webHostEnvironment = webHostEnvironment;
         //}
 
-        public IActionResult Privacy()
+        public IActionResult Index()
         {
             return View();
         }
+
+        //public IActionResult Privacy()
+        //{
+        //    return View();
+        //}
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
@@ -39,7 +35,7 @@ namespace Search_for_a_medicine_by_the_photo_of_its_packaging.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
-        [HttpPost]
+       
         public string TextRecognising(Image image)
         {
             if (image.PackingImage != null)
@@ -54,19 +50,27 @@ namespace Search_for_a_medicine_by_the_photo_of_its_packaging.Controllers
             {
                 return "Ошибка!";
             }
-            Index();
+
+            //IndexF();
         }
 
-        public async Task<IActionResult> Index()
+
+        public async Task<IActionResult> Privacy()
         {
+            Image image = new Image();
+            image.AvailabilityOfInformation = true;
             string json;
             string token = "aII4Hhj1EaeQ";
-            using (var httpClient = new HttpClient())
-            {
-                httpClient.DefaultRequestHeaders.Add("X-Token", token);
-                json = await httpClient.GetStringAsync("http://www.vidal.ru/api/rest/v1/product/list?filter[name]=Аспирин&page=1&limit=5");
-            }
-            return View();
+            using var httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Add("X-Token", token);
+            json = await httpClient.GetStringAsync(@"http://www.vidal.ru/api/rest/v1/product/list?filter[name]=Цитрамон");
+            var jsonString = System.IO.File.ReadAllText("D://Аня//Диплом//Graduate work//Search for a medicine by the photo of its packaging//Product.json") ;
+            var company1 = JsonConvert.DeserializeObject<FileJson.Company1>(jsonString);
+            var company = JsonConvert.DeserializeObject<FileJson.Company>(jsonString);
+            var product = JsonConvert.DeserializeObject<FileJson.Product>(jsonString);
+            var rootobject = JsonConvert.DeserializeObject<FileJson.Rootobject>(jsonString);
+            image.DD = rootobject.products[1].companies[0].company.name.ToString();
+            return View(image); 
         }
     }
 }
